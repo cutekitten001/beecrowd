1b2f502ddee4c1602b535be634de3dd344aa6e48
Danilo Aguiar <79465051+cutekitten001@users.noreply.github.com>
2024-03-12 15:16:42 -0300

    Create BEE 1007.java

---
+++ Iniciante/BEE 1007.java
@@ -0,0 +1,27 @@
+/*
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        int diff = A * B - C * D;
+        
+        System.out.println("DIFERENCA = " + diff);
+        sc.close();
+    }
+}
